6badbda649ed56d1ee61418f63206de74479ef04
alessandra martera <ale.martera@gmail.com>
2023-12-01 17:51:25 +0100

    update timer

--- src/components/TheMain.jsx
+++ src/components/TheMain.jsx
@@ -1,5 +1,5 @@
 import Card from './Card'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 
 
@@ -8,6 +8,19 @@ const imageArray = ['ironman.jpg','marvelcomics.jpg','mountain.jpg','spiderman.j
 
 let [counter, setCounter] = useState(0)
 
+useEffect(() => {
+  const intervalId = setInterval(() => {
+    if (counter < imageArray.length - 1) {
+      setCounter((prevCounter) => prevCounter + 1);
+    } else {
+      setCounter(0);
+    }
+  }, 5000);
+
+  return () => clearInterval(intervalId); 
+}, [counter, imageArray.length]);
+
+/*
 function timerSlide(){
   setInterval(() => {
     if(counter < (imageArray.length - 1)){
@@ -15,22 +28,28 @@ function timerSlide(){
     }else{
       setCounter(0);
     }
-  }, 2000);
+  }, 5000);
 }
-        timerSlide();
+*/
+
+// timerSlide();
       
     return (
       <>
      
        <main>
         <div className='centralContainer'>
-          <button className={"button prev " + (counter === 0 && "hidden")} onClick={() => setCounter(counter - 1)}>Prev</button>
+          <button className={"button prev " + (counter === 0 && "hidden")} onClick={() => {
+            setCounter(counter - 1);
+            }}>Prev</button>
             {imageArray.map((image, idx) => <Card key={idx} image={image} idx={idx} counter={counter} /> )}
           <button className={"button next " + (counter === 3 && "hidden")}  onClick={() => {setCounter(counter + 1)}}>next</button>
         </div>
          
         <div className='dotList'>
-          {imageArray.map((image, idx) =><div key={idx} className={"dot " + (idx===counter && "active")} onClick={() => setCounter(idx)}></div> )}
+          {imageArray.map((image, idx) =><div key={idx} className={"dot " + (idx===counter && "active")} onClick={() => {
+            setCounter(idx);
+            }}></div> )}
         </div>
        </main>
       </>
