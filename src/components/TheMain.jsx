37e112d797e41c3e69b4e39bb172939bfed37c62
alessandra martera <ale.martera@gmail.com>
2023-12-01 16:48:00 +0100

    nav w/ bullets

--- src/components/TheMain.jsx
+++ src/components/TheMain.jsx
@@ -11,10 +11,14 @@ let [counter, setCounter] = useState(0)
     return (
       <>
        <main>
-        <div>
-          <button className={"button " + (counter === 0 && "hidden")} onClick={() => setCounter(counter - 1)}>Prev</button>
+        <div className='centralContainer'>
+          <button className={"button prev " + (counter === 0 && "hidden")} onClick={() => setCounter(counter - 1)}>Prev</button>
             {imageArray.map((image, idx) => <Card key={idx} image={image} idx={idx} counter={counter} /> )}
-          <button className={"button " + (counter === 3 && "hidden")}  onClick={() => {setCounter(counter + 1)}}>next</button>
+          <button className={"button next " + (counter === 3 && "hidden")}  onClick={() => {setCounter(counter + 1)}}>next</button>
+        </div>
+         
+        <div className='dotList'>
+          {imageArray.map((image, idx) =><div key={idx} className={"dot " + (idx===counter && "active")} onClick={() => setCounter(idx)}></div> )}
         </div>
        </main>
       </>
