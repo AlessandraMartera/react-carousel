7678e5cd2048dbb03082d708528b7272c142eccc
alessandra martera <ale.martera@gmail.com>
2023-12-01 17:25:41 +0100

    Autoplay

--- src/components/TheMain.jsx
+++ src/components/TheMain.jsx
@@ -8,8 +8,20 @@ const imageArray = ['ironman.jpg','marvelcomics.jpg','mountain.jpg','spiderman.j
 
 let [counter, setCounter] = useState(0)
 
+function timerSlide(){
+  setInterval(() => {
+    if(counter < (imageArray.length - 1)){
+      setCounter(counter + 1);
+    }else{
+      setCounter(0);
+    }
+  }, 2000);
+}
+        timerSlide();
+      
     return (
       <>
+     
        <main>
         <div className='centralContainer'>
           <button className={"button prev " + (counter === 0 && "hidden")} onClick={() => setCounter(counter - 1)}>Prev</button>
